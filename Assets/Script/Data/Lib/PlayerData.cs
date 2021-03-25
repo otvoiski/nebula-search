c420fe1938a8fd8afb261408065c9289b0f9d689
Otavio Voiski <otvoiski@live.com>
2021-03-25 00:41:37 -0300

    fixed #37

--- Assets/Script/Data/Lib/PlayerData.cs
+++ Assets/Script/Data/Lib/PlayerData.cs
@@ -14,20 +14,5 @@ public class PlayerData : MonoBehaviour
 
     private void Update()
     {
-        Moviment();
-        //Rotation();
-    }
-
-    private void Rotation()
-    {
-        transform.LookAt(_target);
-    }
-
-    private void Moviment()
-    {
-        if (Input.GetKey(KeyCode.W)) _player.Translate(new Vector3(0, 0, Speed * Time.deltaTime));
-        if (Input.GetKey(KeyCode.S)) _player.Translate(new Vector3(0, 0, -(Speed * Time.deltaTime)));
-        if (Input.GetKey(KeyCode.D)) _player.Translate(new Vector3(Speed * Time.deltaTime, 0, 0));
-        if (Input.GetKey(KeyCode.A)) _player.Translate(new Vector3(-(Speed * Time.deltaTime), 0, 0));
     }
 }
